be0f117c0fba97da94476e12e5a61a04a8d6bc22
Parker Herlache <pa03192@intranet.secura.net>
2025-10-22 17:25:10 -0500

    - add sample data - display data on UI - add basic css styling

--- Controllers/ApiSourceController.cs
+++ Controllers/ApiSourceController.cs
@@ -7,11 +7,19 @@ public class ApiSourceController : Controller
 {
     public ApiSourceController()
     {
-        //does nothing
+        //empty constructor
     }
     
     public IActionResult ApiSource()
     {
-        return View();
+        //create hardcoded api data
+        List<ApiSource> _apis = new()
+        {
+            new ApiSource { Id = 1, Name = "Ageify.io", Url="https://api.agify.io?name=Parker", Description="Predicts a persons age based on their name" },
+            new ApiSource { Id = 2, Name = "Data Usa", Url="https://datausa.io/api/data?drilldowns=Nation&measures=Population", Description="Gets USA Population"},
+            new ApiSource { Id = 3, Name = "Data Usa", Url="https://datausa.io/api/data?drilldowns=Nation&measures=Population", Description="Gets USA Population"}
+        };
+
+        return View(_apis);
     }
 }
